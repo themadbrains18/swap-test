c04c1baf07744cc7adda6bb62cbc35d8361d0b3f
surinder <surinderkumar.mdb@gmail.com>
2024-01-01 18:20:55 +0530

    build push

---
+++ static/js/6.35ea1dfb.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpundefined=this.webpackJsonpundefined||[]).push([[6],{704:function(n,i){},711:function(n,i){},713:function(n,i){}}]);
+//# sourceMappingURL=6.35ea1dfb.chunk.js.map
---
+++ static/js/7.98198337.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpundefined=this.webpackJsonpundefined||[]).push([[7],{664:function(n,e){}}]);
+//# sourceMappingURL=7.98198337.chunk.js.map
